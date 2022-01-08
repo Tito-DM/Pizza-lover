cb788102b45b740540495c9b7535a5830623a1b0
pyong <muanda1993@hotmail.com>
2022-01-08 23:01:55 +0000

    complete navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/NavBar.module.css";
+import { AiOutlinePhone, AiOutlineShoppingCart } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.callButton}>
+            <AiOutlinePhone size={32} />
+          </div>
+          <div className={styles.texts}>
+            <div className={styles.text}>ORDER NOW!</div>
+            <div className={styles.text}> 012 345 678</div>
+          </div>
+        </div>
+
+        <div className={styles.item}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>HomePage</li>
+            <li className={styles.listItem}>Products</li>
+            <li className={styles.listItem}>Menu</li>
+            <h1>Pizza´s Loover</h1>
+            <li className={styles.listItem}>Events</li>
+            <li className={styles.listItem}>contact</li>
+          </ul>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <AiOutlineShoppingCart size={32} />
+            <div className={styles.counter}>2</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
